d246eadb32193d85c02a832a7035867a63c45140
Marek Ceľuch <celuchmarek@gmail.com>
2024-08-05 16:16:07 +0200

    Autoload unless xsd and xslt (#20)
    * default to autoload unless xlst and xsd specified
    
    * minor cleanup

--- app/controllers/api/v1/documents_controller.rb
+++ app/controllers/api/v1/documents_controller.rb
@@ -105,7 +105,7 @@ class Api::V1::DocumentsController < ApplicationController
       params.require(:document).require(:content)
 
       params[:parameters] = {} unless params[:parameters]
-      params[:parameters][:autoLoadEform] = true unless params[:parameters][:containerXmlns]
+      params[:parameters][:autoLoadEform] = true unless params[:schema] && params[:transformation]
 
       params.permit(
         :encryption_key,
