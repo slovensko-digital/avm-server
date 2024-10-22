45d1118c64186d6de4fc50002f6f8224887552ea
Marek Ceľuch <celuchmarek@gmail.com>
2024-10-22 15:23:22 +0200

    fix typo

--- app/controllers/assetlinks_controller.rb
+++ app/controllers/assetlinks_controller.rb
@@ -4,6 +4,6 @@ class AssetlinksController < ApplicationController
   end
 
   def android_assetlinks
-    render :json => JSON.load(Base64.decode64 ENV.fetch('ANDROID_ASSEtLINKS', 'e30='))
+    render :json => JSON.load(Base64.decode64 ENV.fetch('ANDROID_ASSETLINKS', 'e30='))
   end
 end
